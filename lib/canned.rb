d040a6db53f8601fa9c44489f985dd6fdc9882d8
Ignacio Baixas <iobaixas@gmai.com>
2012-12-04 14:07:51 -0300

    removes action controller presence check

--- lib/canned.rb
+++ lib/canned.rb
@@ -3,4 +3,4 @@ require "canned/version"
 require "canned/errors"
 require "canned/test_context"
 require "canned/definition"
-require "canned/rails" if defined? ActionController
+require "canned/rails"
